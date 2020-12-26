e8f0166201f9b1173c39595215e93118ca80bf6c
Florian Leon <41922514+florianleon@users.noreply.github.com>
2020-12-26 23:11:55 +0100

    Update

--- POO/ChatSystem GUI/src/chatSytem/view/ChatController.java
+++ POO/ChatSystem GUI/src/chatSytem/view/ChatController.java
@@ -6,13 +6,10 @@ import java.util.ArrayList;
 
 import chatSytem.Main;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.stage.Stage;
 import src.database.ActiveUsersDB;
 import src.user.Login;
 import javafx.scene.control.Button;
@@ -20,9 +17,9 @@ import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 
 public class ChatController {
 	
@@ -49,10 +46,10 @@ public class ChatController {
 	private Label dateLabel;
 	
 	@FXML
-	private TableView<Button> tableView;
+	private TableView<Label> tableView;
 	
-	@FXML
-	private TableColumn tableViewColumn;
+	//@FXML
+	//private TableColumn<Button, String> tbc;
 	
 	@FXML 
 	private ButtonBar buttonBar;
@@ -78,7 +75,18 @@ public class ChatController {
 	@FXML
 	private Menu status;
 	
+	
 	Alert a = new Alert(AlertType.NONE); 
+	
+	/*
+	 * @FXML private void initialize() { // Create column UserName (Data type of
+	 * String). tbc = new TableColumn<Button, String>("Users"); // Defines how to
+	 * fill data for each cell. // Get value from property of UserAccount. .
+	 * tbc.setCellValueFactory(new PropertyValueFactory<>("Users")); // Set Sort
+	 * type for userName column tbc.setSortType(TableColumn.SortType.DESCENDING); //
+	 * Display row data //ObservableList<Button> list = null;
+	 * //tableView.setItems(list); tableView.getColumns().addAll(tbc); }
+	 */
 	
 	
 	//Method to display and hide chooseUser, endConvoButton and button bar
@@ -159,26 +167,47 @@ public class ChatController {
 	
 	
 	//TODO : rajouter observeur
+	/*
+	 * @FXML public void updateTable2() throws ClassNotFoundException, SQLException
+	 * { activeUsers = Main.user.getActiveUsers(); ArrayList<Login> list =
+	 * activeUsers.getAllUsers(); ObservableList<Button> list2 =
+	 * tableView.getItems(); for(Login l : list) { Button btn = new Button();
+	 * btn.setMinWidth(328); btn.setMinHeight(50); btn.setVisible(true);
+	 * btn.setText(l.getLogin()); btn.setOnAction(new EventHandler<ActionEvent>() {
+	 * 
+	 * @Override public void handle(ActionEvent event) {
+	 * System.out.println(btn.getText()); //Or "1" as sin your code } });
+	 * list2.add(btn); System.out.println(list2.toString()); tbc.setVisible(true); }
+	 * //return list2;
+	 * 
+	 * }
+	 */
+	
 	@FXML
 	public void updateTable() throws ClassNotFoundException, SQLException {
 		activeUsers = Main.user.getActiveUsers();
 		ArrayList<Login> list = activeUsers.getAllUsers();
-		
 		for(Login l : list) {
-			Button btn = new Button(); 
-			btn.setMinWidth(328);
-            btn.setMinHeight(50);
-            btn.setText(l.getLogin());  
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-                @Override 
-                public void handle(ActionEvent event) {
-                    System.out.println(btn.getText()); //Or "1" as in your code
-                }
+			Label name = new Label();
+			name.setPrefWidth(328);
+			name.setPrefHeight(50);
+			name.setMinWidth(328);
+			name.setMinHeight(50);
+			name.setMaxWidth(328);
+			name.setMaxHeight(50);
+			name.setText(l.getLogin());
+			name.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent arg0) {
+					System.out.println("test");
+					
+				}
             });
-            tableView.getItems().add(btn);
+			tableView.getItems().add(name);
 		}
-		
 	}
+	
+	
 	
 	//NE marche pas encore : regarder https://stackoverflow.com/questions/12153622/how-to-close-a-javafx-application-on-window-close 
 	@FXML
