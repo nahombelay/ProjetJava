e3810f49e8defc7401fbf5f370764f6435b2facd
Nahom <asegednahom@gmail.com>
2020-12-26 20:29:22 +0100

    HTTP + Webscraping
    Avancer sur la partie http (inserton dans la BDD est bon)
    Webscrapping pratiquement terminée (arrive à lire tableau html et mettre contenu dans BDD et arrive à lire le message qui a été forward par le serveur)
    Faut faire la partie proxy avec lequel j'ai un peu de mal à voir comment faire

---
+++ POO/ChatSystem Server/WebContent/WEB-INF/UpdateInternalUser.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.net.URL, com.database.UsersDatabaseServer"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Update Internal User</title>
+	</head>
+	<body>
+		<%! String ip = ""; 
+			String username = "";
+			String status = "";
+			String URL = "";
+		%>
+		
+		<p>Inserstion des elements dans la base de données</p>
+		
+		<p> 
+		<% 
+		
+		ip = (String) request.getParameter("ip");
+		username = (String) request.getParameter("username");
+		status = (String) request.getParameter("status");
+		URL = new URL(request.getRequestURL().toString()).getHost(); 
+		
+		if (status.equals("offline")){
+			new UsersDatabaseServer().deleteUser(ip, username, URL, "InternalUsers");
+			out.println("User has been deleted");
+		} else {
+			new UsersDatabaseServer().updateUser(ip, username, status, URL, "InternalUsers");
+			out.println("User has been updated");
+		}
+		%>
+		</p>
+		
+	</body>
+</html>
--- POO/ChatSystem Server/src/com/database/UsersDatabaseServer.java
+++ POO/ChatSystem Server/src/com/database/UsersDatabaseServer.java
@@ -14,12 +14,15 @@ public class UsersDatabaseServer {
 	private ResultSet rs = null;
 	@SuppressWarnings("unused")
 	private int rowsModified = 0;
+	private String username = "tp_servlet_016";
+    private String password = "aiTooN5b";
 	
 	public UsersDatabaseServer() {
-		String url = "jdbc:sqlite://localhost:3306/UsersDatabaseServer.db";
+		
+		String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/tp_servlet_016?useSSL=false";
 		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection(url);
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c = DriverManager.getConnection(url, username, password);
 			System.out.println("[" + this.getClass().toString() + "] Connection OK");
 	
 		} catch ( SQLException | ClassNotFoundException  e) {
@@ -28,7 +31,7 @@ public class UsersDatabaseServer {
 		}
 	}
 	
-	public void addUser(String ip, String username, String status,  String table, String URL) {
+	public void addUser(String ip, String username, String status, String URL, String table) {
 		String query = "INSERT INTO " + table + " (ip, username, status, URL) VALUES (?,?,?,?);";
 		
 		try {
@@ -46,13 +49,14 @@ public class UsersDatabaseServer {
 		}
 	}
 	
-	public void deleteUser(String ip, String username,  String table) {
-		String query = "DELETE FROM " + table + " WHERE ip = ? OR username = ?;";
+	public void deleteUser(String ip, String username, String URL, String table) {
+		String query = "DELETE FROM " + table + " WHERE ip = ? OR username = ? OR URL = ?;";
 		
 		try {
 			this.pstm = c.prepareStatement(query);
 			this.pstm.setString(1, ip);
 			this.pstm.setString(2, username);
+			this.pstm.setString(3, URL);
 			this.rowsModified = pstm.executeUpdate();
 			this.pstm.close();
 			
@@ -62,14 +66,15 @@ public class UsersDatabaseServer {
 		}
 	}
 	
-	public void updateUser(String ip, String username, String status, String table) {
-		String query = "UPDATE " + table + " SET username = ?, status = ? WHERE ip = ?;";
+	public void updateUser(String ip, String username, String status, String URL, String table) {
+		String query = "UPDATE " + table + " SET username = ?, status = ? WHERE ip = ? OR URL = ?;";
 		
 		try {
 			this.pstm = c.prepareStatement(query);
 			this.pstm.setString(2, status);
 			this.pstm.setString(1, username);
 			this.pstm.setString(3, ip);
+			this.pstm.setString(4, URL);
 			this.rowsModified = pstm.executeUpdate();
 			
 			this.pstm.close();
@@ -80,9 +85,30 @@ public class UsersDatabaseServer {
 		}
 	}
 	
+	public Connection getConnection(String table) {
+		return this.c;
+	}
 	public static void main (String [] agrv) {
 		UsersDatabaseServer db = new UsersDatabaseServer();
-		//db.deleteUser("1.1.1.1", "toto", "InternalUsers");
+		Connection test = db.getConnection("InternUser");
+		try {
+			Statement s = test.createStatement();
+			ResultSet rset = s.executeQuery("SELECT * FROM InternalUsers");
+			
+			while (rset.next()) {
+				String ip = rset.getString(1);
+				String username = rset.getString(2);
+				
+				System.out.println(ip + " : " + username);
+			}
+			
+			s.close();
+			rset.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
